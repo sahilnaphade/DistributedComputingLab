e6a9b025df12868c366c8002865b753e7138487a
ItsNaSa <naphadesahil@gmail.com>
2021-02-01 16:44:01 +0530

    TCP Client-server added

--- Assignment1TCP/src/clientPackage/Client.java
+++ Assignment1TCP/src/clientPackage/Client.java
@@ -1,41 +1,37 @@
 package clientPackage;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 import java.net.*;
 import java.util.*;
 
 class Client {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		Integer portNo;
 		Scanner scn1 = new Scanner(System.in);
-		String toSend;
-		try {
-			System.out.println("Enter the port no on which you want to connect: ");
-			portNo = scn1.nextInt();
-			InetAddress ip = InetAddress.getByName("localhost");
-			Socket s = new Socket(ip, portNo);
-			DataInputStream dis = new DataInputStream(s.getInputStream());
-			DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-			while(true) {
-				System.out.println(dis.readUTF());
-				toSend = scn1.nextLine();
-				dos.writeUTF(toSend);
-				if(toSend.equals("Exit")) {
-					System.out.println("Closing the connection: " + s);
-					s.close();
-					System.out.println("Connection closed");
-					break;
-				}
-				else {
-					String receivedData = dis.readUTF();
-					System.out.println(receivedData);
-				}
-			}
+		String toSend = "", recieved = "";
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.print("Enter the port no on which you want to connect: ");
+		portNo = scn1.nextInt();
+		Socket s = new Socket("localhost", portNo);
+		
+		DataInputStream dis = new DataInputStream(s.getInputStream());
+		DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+		
+		while(!toSend.equals("Exit")) {
+			System.out.print("Enter your message: ");
+			toSend = br.readLine();
+			dos.writeUTF(toSend);
+			dos.flush();
+			recieved = dis.readUTF();
+			System.out.println("Server: " + recieved);
 		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
+		dos.close();
+		dis.close();
+		s.close();
 		scn1.close();
 	}
 }
--- Assignment1TCP/src/serverPackage/Server.java
+++ Assignment1TCP/src/serverPackage/Server.java
@@ -9,36 +9,29 @@ import java.net.*;
 public class Server {
 	public static void main(String[] args) throws IOException {
 		Scanner scn1 = new Scanner(System.in);
-		String receivedData, dataToTransmit;
+		String receivedData = "", dataToTransmit = "";
 		System.out.print("Enter the port no. on which you wish to start server: ");
 		int portNo = scn1.nextInt();
-		
 		ServerSocket ss = new ServerSocket(portNo);
 		System.out.println("Server waiting on port no. " + portNo);
-		Socket s = null;
-		while(true) {
-			try {
-				s = ss.accept();
-				DataInputStream dis = new DataInputStream(s.getInputStream());
-				DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-				receivedData = dis.readUTF();
-				if(receivedData.equals("Exit")) {
-					System.out.println("Client "+ s +" is exiting!");
-					s.close();
-					System.out.println("Connection closed!!");
-					break;
-				}
-				else {
-					System.out.println(receivedData);
-					dataToTransmit = scn1.nextLine();
-					dos.writeUTF(dataToTransmit);
-				}
-			}
-			catch(Exception e1) {
-				e1.printStackTrace();
-			}
-			ss.close();
-			scn1.close();
+		Socket s = ss.accept();
+		DataInputStream dis = new DataInputStream(s.getInputStream());
+		DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		while(!receivedData.equals("Exit")) {
+			receivedData = dis.readUTF();
+			System.out.println("Client: " + receivedData);
+			System.out.print("Enter your response: ");
+			dataToTransmit = br.readLine();
+			dos.writeUTF(dataToTransmit);
+			dos.flush();
 		}
+		dis.close();
+		dos.close();
+		s.close();
+		ss.close();
+		scn1.close();
 	}
 }
