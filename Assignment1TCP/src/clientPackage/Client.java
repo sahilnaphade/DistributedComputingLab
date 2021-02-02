dc7318ededfc5692ed2822d52a334187d3788668
Sahil Naphade <38909890+ItsNaSa@users.noreply.github.com>
2021-02-02 22:24:44 +0530

    Tried RMI

--- Assignment1TCP/src/clientPackage/Client.java
+++ Assignment1TCP/src/clientPackage/Client.java
@@ -9,26 +9,27 @@ import java.util.*;
 
 class Client {
 	public static void main(String[] args) throws Exception{
-		Integer portNo;
-		Scanner scn1 = new Scanner(System.in);
+		Integer portNo;	// To accept the port number to connect the client
+		Scanner scn1 = new Scanner(System.in);	// To converse with the server
 		String toSend = "", recieved = "";
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // To read the lines in a buffer to send
 		
 		System.out.print("Enter the port no on which you want to connect: ");
-		portNo = scn1.nextInt();
-		Socket s = new Socket("localhost", portNo);
+		portNo = scn1.nextInt();	// Accept port no.
+		Socket s = new Socket("localhost", portNo);	// Create a socket with the selected host and port
 		
-		DataInputStream dis = new DataInputStream(s.getInputStream());
-		DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-		
+		DataInputStream dis = new DataInputStream(s.getInputStream());	// DataInputStream object with the concerned socket to accept the message from server
+		DataOutputStream dos = new DataOutputStream(s.getOutputStream());	// DataOutputStream object with the concerned socket to send the message to server
+		// Run while the client does not send "Exit		
 		while(!toSend.equals("Exit")) {
-			System.out.print("Enter your message: ");
-			toSend = br.readLine();
-			dos.writeUTF(toSend);
-			dos.flush();
-			recieved = dis.readUTF();
-			System.out.println("Server: " + recieved);
+			System.out.print("Enter your message: ");	// Accept message from Client
+			toSend = br.readLine();	
+			dos.writeUTF(toSend); // Write the message in UTF format to send to server
+			dos.flush();	// Clear the dos for further communication
+			recieved = dis.readUTF();	// receive the response from the server
+			System.out.println("Server: " + recieved);	// print the response
 		}
+		// Close all of open sockets, scanner and stream objects
 		dos.close();
 		dis.close();
 		s.close();
--- Assignment1TCP/src/serverPackage/Server.java
+++ Assignment1TCP/src/serverPackage/Server.java
@@ -8,26 +8,27 @@ import java.net.*;
 
 public class Server {
 	public static void main(String[] args) throws IOException {
-		Scanner scn1 = new Scanner(System.in);
+		Scanner scn1 = new Scanner(System.in);// To converse with the client
 		String receivedData = "", dataToTransmit = "";
 		System.out.print("Enter the port no. on which you wish to start server: ");
-		int portNo = scn1.nextInt();
-		ServerSocket ss = new ServerSocket(portNo);
+		int portNo = scn1.nextInt();// To accept the port number to start the server
+		ServerSocket ss = new ServerSocket(portNo); // Create a server socket with the said port no
 		System.out.println("Server waiting on port no. " + portNo);
 		Socket s = ss.accept();
-		DataInputStream dis = new DataInputStream(s.getInputStream());
-		DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+		DataInputStream dis = new DataInputStream(s.getInputStream());// DataInputStream object with the concerned socket to accept the message from client
+		DataOutputStream dos = new DataOutputStream(s.getOutputStream());// DataOutputStream object with the concerned socket to send the message to client
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		while(!receivedData.equals("Exit")) {
-			receivedData = dis.readUTF();
-			System.out.println("Client: " + receivedData);
-			System.out.print("Enter your response: ");
+			receivedData = dis.readUTF(); // Accept message from Client
+			System.out.println("Client: " + receivedData);	// print
+			System.out.print("Enter your response: ");	// Write the message in UTF format to send to client
 			dataToTransmit = br.readLine();
-			dos.writeUTF(dataToTransmit);
+			dos.writeUTF(dataToTransmit); // Send the message
 			dos.flush();
 		}
+//		Close the connections and streams
 		dis.close();
 		dos.close();
 		s.close();
