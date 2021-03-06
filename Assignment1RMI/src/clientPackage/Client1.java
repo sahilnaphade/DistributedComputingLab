a8c8b1c2a25ed791d2c12926439582d97821eaa9
ItsNaSa <naphadesahil@gmail.com>
2021-03-06 13:28:09 +0530

    Resolved RMIregistry issue

--- Assignment1RMI/src/clientPackage/Client1.java
+++ Assignment1RMI/src/clientPackage/Client1.java
@@ -13,10 +13,12 @@ public class Client1 {
 			Scanner s = new Scanner(System.in);
 			Hasher stub = (Hasher) Naming.lookup("rmi://127.0.0.1:5000/hash");
 			
-			System.out.print("Enter the choice of algorithm:\n1 for SHA-256\n2 for SHA-512\n3 for MD5\nYour choice :: ");
-			choice = s.nextInt();
 			System.out.print("Enter the string to be hashed : ");
 			data = s.nextLine();
+
+			System.out.print("Enter the choice of algorithm:\n1 for SHA-256\n2 for SHA-512\n3 for MD5\nYour choice :: ");
+			choice = s.nextInt();
+			
 			
 			System.out.println(stub.createHash(data, choice));			
 		}
--- Assignment1RMI/src/serverPackage/HashServer.java
+++ Assignment1RMI/src/serverPackage/HashServer.java
@@ -1,12 +1,14 @@
 package serverPackage;
 
 import java.rmi.*;
+import java.rmi.registry.*;
 import serverPackage.*;
 
 public class HashServer {
 	public static void main(String[] args) {
 		try {
 			Hasher stub = new HasherRemote();
+			LocateRegistry.createRegistry(5000);
 			Naming.rebind("rmi://127.0.0.1:5000/hash", stub);
 		}
 		catch(Exception E) {
